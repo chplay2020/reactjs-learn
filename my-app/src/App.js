a98543aaf5810eca3b7d3488fe237813d23e3498
Thành Trần <ratosan19@example.com>
2025-05-20 01:35:31 +0700

    init project

---
+++ my-app/src/App.js
@@ -0,0 +1,34 @@
+
+import './App.css';
+import MyComponent from './components/MyComponent';
+
+
+const App = () => {
+  return (
+    <div>
+      hello world
+      <MyComponent></MyComponent>
+    </div>
+  );
+}
+
+
+export default App;
+
+
+// <div className="App">
+//   <header className="App-header">
+//     <img src={logo} className="App-logo" alt="logo" />
+//     <p>
+//       Edit <code>src/App.js</code> and save to reload.
+//     </p>
+//     <a
+//       className="App-link"
+//       href="https://reactjs.org"
+//       target="_blank"
+//       rel="noopener noreferrer"
+//     >
+//       Learn React
+//     </a>
+//   </header>
+// </div>
